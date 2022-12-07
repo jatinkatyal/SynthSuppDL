4ad2fe1bdd77041f37106b5af60ef2e0f4bb4608
Jatin Katyal <jatinkatyal96@gmail.com>
2022-12-07 18:11:23 -0500

    jobs for more detectors.

---
+++ jobs/UAVDT-AirSim/r20as10b_detector.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=16:00:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=14000M
+#SBATCH --gres=gpu:1
+
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+
+# module load python/3.9 cuda cudnn scipy-stack opencv
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+unzip -q /home/jatin/datasets/UAVDT/UAV-benchmark-M.zip -d $SLURM_TMPDIR/UAVDT/
+cp -r /home/jatin/datasets/UAVDT/UAV-benchmark-MOTD_v1.0 $SLURM_TMPDIR/UAVDT/UAV-benchmark-MOTD_v1.0
+unzip -q /home/jatin/datasets/AirSim-UAV-Synth.zip -d $SLURM_TMPDIR/airsim
+
+# source /home/jatin/ENV/bin/activate
+python /home/jatin/work/tracking_analysis/src/utils/traktor_objdet.py --dataset=$SLURM_TMPDIR/UAVDT/ --synth_dataset=$SLURM_TMPDIR/airsim/data/ --model_prefix=R20aS10b --mode="train20-a" --synthmode="10b"
+
+exit
