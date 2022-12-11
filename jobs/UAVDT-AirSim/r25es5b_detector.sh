641ebd0ff3b28b7040a2f98d886904567852f486
Jatin Katyal <jatinkatyal96@gmail.com>
2022-12-11 18:13:32 -0500

    more re-ids and dets.

---
+++ jobs/UAVDT-AirSim/r25es5b_detector.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=8:00:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=14000M
+#SBATCH --gres=gpu:1
+#SBATCH --output="/home/jatin/scratch/slurm-%j.out"
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+
+# module load python/3.9 cuda cudnn scipy-stack opencv
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+unzip -q /home/jatin/datasets/UAVDT/UAV-benchmark-M.zip -d $SLURM_TMPDIR/UAVDT/
+cp -r /home/jatin/datasets/UAVDT/UAV-benchmark-MOTD_v1.0 $SLURM_TMPDIR/UAVDT/UAV-benchmark-MOTD_v1.0
+unzip -q /home/jatin/datasets/airsim/AirSim-UAV-Synth.zip -d $SLURM_TMPDIR/airsim
+
+# source /home/jatin/ENV/bin/activate
+python /home/jatin/work/tracking_analysis/src/utils/traktor_objdet.py --dataset=$SLURM_TMPDIR/UAVDT/ --synth_dataset=$SLURM_TMPDIR/airsim/data/ --model_prefix=R25eS5b --mode="train25-e" --synthmode="5b" --model=/home/jatin/models/R25eS5b_epoch_22.model
+
+exit
---
+++ jobs/UAVDT-AirSim/r25es5b_reid.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --time=15:00:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=10000M
+#SBATCH --gres=gpu:1
+#SBATCH --output="/home/jatin/scratch/slurm-%j.out"
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+
+# module load python/3.9 cuda cudnn scipy-stack opencv
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+unzip -q /home/jatin/datasets/UAVDT/UAVDT_reid.zip -d $SLURM_TMPDIR/
+unzip -q /home/jatin/datasets/airsim/airsim-reid-data.zip -d $SLURM_TMPDIR/AirSim-reid-data/
+
+# cp -r /home/jatin/datasets/AirSim-UAV-Synth/reid-data $SLURM_TMPDIR/airsim_reid
+# source /home/jatin/ENV/bin/activate
+
+python /home/jatin/work/tracking_analysis/src/utils/traktor_reid.py --data=$SLURM_TMPDIR/UAVDT_reid --syndata=$SLURM_TMPDIR/AirSim-reid-data/reid-data --mode=R25eS5b
+exit
