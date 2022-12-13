7923facdfff028a2e3d96d21b261f99f591c7ce9
Jatin Katyal <jatinkatyal96@gmail.com>
2022-12-13 18:23:03 -0500

    more detector jobs.

---
+++ jobs/UAVDT-AirSim/r5as25_detector.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --time=22:00:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=14000M
+#SBATCH --gres=gpu:1
+#SBATCH --output="/home/jatin/scratch/slurm-%j.out"
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+
+# module load python/3.9 cuda cudnn scipy-stack opencv
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+unzip -q /home/jatin/datasets/UAVDT/UAV-benchmark-M.zip -d $SLURM_TMPDIR/UAVDT/
+cp -r /home/jatin/datasets/UAVDT/UAV-benchmark-MOTD_v1.0 $SLURM_TMPDIR/UAVDT/UAV-benchmark-MOTD_v1.0
+unzip -q /home/jatin/datasets/airsim/AirSim-UAV-Synth.zip -d $SLURM_TMPDIR/airsim
+unzip -q datasets/airsim/additional-synth-sequences.zip -d $SLURM_TMPDIR/airsim/data/Sequences/
+unzip -q datasets/airsim/additional-synth-annots.zip -d $SLURM_TMPDIR/airsim/data/Annotations/
+unzip -q datasets/airsim/additional-synth-sequences2.zip -d $SLURM_TMPDIR/airsim/data/Sequences/
+unzip -q datasets/airsim/additional-synth-annots2.zip -d $SLURM_TMPDIR/airsim/data/Annotations/
+
+# source /home/jatin/ENV/bin/activate
+python /home/jatin/work/tracking_analysis/src/utils/traktor_objdet.py --dataset=$SLURM_TMPDIR/UAVDT/ --synth_dataset=$SLURM_TMPDIR/airsim/data/ --model_prefix=R5aS25 --mode="train5-a" --synthmode="25"
+
+exit
