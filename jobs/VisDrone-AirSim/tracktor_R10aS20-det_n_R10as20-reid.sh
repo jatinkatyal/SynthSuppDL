d335a3d07db7167f543b1091d1b2030adff20313
Jatin Katyal <jatinkatyal96@gmail.com>
2022-12-28 18:47:42 -0500

    Results generation job for VisDrone-AirSim combo with Tracktor.

---
+++ jobs/VisDrone-AirSim/tracktor_R10aS20-det_n_R10as20-reid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=10000M
+#SBATCH --gres=gpu:1
+#SBATCH --output="/home/jatin/scratch/slurm-%j.out"
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+unzip -q /home/jatin/datasets/VisDrone/VisDrone2019-MOT-test-dev.zip -d $SLURM_TMPDIR/
+
+python /home/jatin/work/tracking_analysis/src/tracktor.py --det_model=/home/jatin/models/R10aS20_epoch_30.model --reid_model=/home/jatin/models/tracktor_reid_R10aS20/model/model.pth.tar-10 --config=/home/jatin/work/tracking_analysis/cfgs/tracktor.yaml --dataset=$SLURM_TMPDIR/VisDrone2019-MOT-test-dev --output=/home/jatin/results/tracktor/VisDrone/R10aS20-det_n_R10aS20-reid  --dataset_type='VisDrone'
+
+exit
---
+++ jobs/VisDrone-AirSim/tracktor_R25dS5a-det_n_R25dS5a-reid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=10000M
+#SBATCH --gres=gpu:1
+#SBATCH --output="/home/jatin/scratch/slurm-%j.out"
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+unzip -q /home/jatin/datasets/VisDrone/VisDrone2019-MOT-test-dev.zip -d $SLURM_TMPDIR/
+
+python /home/jatin/work/tracking_analysis/src/tracktor.py --det_model=/home/jatin/models/R25dS5a_epoch_30.model --reid_model=/home/jatin/models/tracktor_reid_R25dS5a/model/model.pth.tar-10 --config=/home/jatin/work/tracking_analysis/cfgs/tracktor.yaml --dataset=$SLURM_TMPDIR/VisDrone2019-MOT-test-dev --output=/home/jatin/results/tracktor/VisDrone/R25dS5a-det_n_R25dS5a-reid  --dataset_type='VisDrone'
+
+exit
