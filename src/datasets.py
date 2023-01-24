0c4d5137e47b5fb3c082bc7344478514697b8e15
Jatin Katyal <jatinkatyal96@gmail.com>
2023-01-23 19:08:32 -0500

    Updated datasets.py file

--- src/datasets.py
+++ src/datasets.py
@@ -4,10 +4,60 @@ from torch.utils.data import Dataset
 from torchvision.io import read_image
 import os
 
-from matplotlib import pyplot as plt
+import matplotlib
+import matplotlib.pyplot as plt
 from matplotlib import patches
 
+class Images(Dataset):
+    def __init__(self,path,seq,dataset_type,transform=None):
+        #print(path,seq,dataset_type)
+        if dataset_type == 'UAVDT':
+            seq_pattern = 'UAV-benchmark-M/'+seq
+            annot_pattern = 'UAV-benchmark-MOTD_v1.0/GT/'+seq+'_gt.txt'
+        elif dataset_type == 'VisDrone':
+            seq_pattern = 'sequences/'+seq
+            annot_pattern = 'annotations/'+seq+'.txt'
+        self.path = path
+        self.seq_path = os.path.join(self.path,seq_pattern)
+        self.img_list = os.listdir(self.seq_path)
+        self.img_list.sort()
+        self.transform = transform
 
+        annotation_path = os.path.join(self.path,annot_pattern)
+        anots = np.loadtxt(annotation_path, delimiter=',')
+        #if dataset_type == 'VisDrone':
+        #    anots = anots[(anots[:,7]==4) | (anots[:,7]==5) | (anots[:,7]==6) | (anots[:,7]==9)] # Filtering vehicles
+        self.annotations = anots
+
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, item):
+        img_path = os.path.join(self.seq_path,self.img_list[item])
+        img = read_image(img_path)/255
+        img = img.type(torch.DoubleTensor)
+        if self.transform:
+            shape_og = img.shape
+            img = self.transform(img)
+            shape_new = img.shape
+
+        anots_ith_frame = self.annotations[self.annotations[:, 0] == item+1]
+        bboxes = [
+            anots_ith_frame[:, 2],
+            anots_ith_frame[:, 3],
+            anots_ith_frame[:, 2] + anots_ith_frame[:, 4],
+            anots_ith_frame[:, 3] + anots_ith_frame[:, 5]
+        ]
+        bboxes = np.array(bboxes)
+        bboxes = torch.tensor(bboxes).T
+
+        print(bboxes.shape)
+        if self.transform:
+            c_og, y_og, x_og = shape_og
+            c_new,y_new,x_new = shape_new
+            print(torch.tensor([x_new/x_og,y_new/y_og,x_new/x_og,y_new/y_og]).shape)
+            bboxes = bboxes*torch.tensor([x_new/x_og,y_new/y_og,x_new/x_og,y_new/y_og])
+        return img, bboxes
 
 class UAVDTSeq(Dataset):
     """
@@ -40,12 +90,12 @@ class UAVDTSeq(Dataset):
         anots = anots[anots[:,0]<=self.framelimit]
 
         bbboxes = []
-        for i in range(1,int(anots[:,0].max())):
+        for i in range(1,int(anots[:,0].max()+1)):
             anots_ith_frame = anots[anots[:,0]==i]
             bboxes_ith_frame = [anots_ith_frame[:,2],
                                 anots_ith_frame[:,3],
-                                anots_ith_frame[:,4],
-                                anots_ith_frame[:,5]]
+                                anots_ith_frame[:,2] + anots_ith_frame[:,4],
+                                anots_ith_frame[:,3] + anots_ith_frame[:,5]]
             bboxes_ith_frame = torch.stack(bboxes_ith_frame).T
             bbboxes.append(bboxes_ith_frame)
 
@@ -62,6 +112,7 @@ class UAVDTSeq(Dataset):
         seq = []
         for img_nm in seq_dir:
             frame = read_image(os.path.join(seq_dir_path,img_nm))/255
+            frame = frame.type(torch.DoubleTensor)
             if self.transform:
                 frame=self.transform(frame)
             frame.req_grads=False
@@ -78,6 +129,7 @@ class UAVDTSeq(Dataset):
         return files_to_keep
 
 def plot(img,boxes,color='red'):
+    matplotlib.use('TkAgg')
     plt.figure()
     plt.imshow(torch.permute(img, (1, 2, 0)))
     ref = plt.gca()
@@ -86,30 +138,28 @@ def plot(img,boxes,color='red'):
         ref.add_patch(bbox)
     plt.show()
 
-def collate_fn(batches):
-    frames,targets = tuple(zip(*batches))
-    frames = torch.stack(frames)
-    return  frames, targets
+def collate_fn(batch):
+    image_list, boxes_list = [], []
+    for image, boxes in batch:
+        image_list.append(image)
+        boxes_list.append(boxes)
+    return image_list, boxes_list
 
 if __name__ == '__main__':
     import argparse
     argsparser = argparse.ArgumentParser(description='Just passing some parameters')
-    argsparser.add_argument('--dataset',
-                            default='/home/jatin/InternalHDD/Work/computerVision/Datasets/UAVDT_reid',
-                            help='path to dataset')
+    argsparser.add_argument('--dataset', help='path to dataset')
     args = argsparser.parse_args()
 
-    '''
     from torchvision.transforms import Compose, Normalize
     transform = Compose([
         Normalize(mean=[0.485, 0.456, 0.406], std=[0.225, 0.225, 0.225])
     ])
-    dataset = UAVDTObjDet(args.dataset, transform=transform)
+    dataset = UAVDTSeq(args.dataset, transform=transform)
 
     import random
     j = random.randint(0, len(dataset))
     img,target = dataset[j]
-    print(img.dtype)
     plot(img,target['boxes'])
 
 
@@ -121,6 +171,4 @@ if __name__ == '__main__':
         print(f'batch:{i}\tframes:{frames.shape}')
         print(f'target: {targets}')
         break
-    '''
-    dataset = UAVDTReId(imgpath=args.dataset)
-    print(dataset[10])
+
