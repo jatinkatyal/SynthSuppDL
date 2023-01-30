d7d2120bb6ce9a0a67de688770baa9f4876ca171
Jatin Katyal <jatinkatyal96@gmail.com>
2023-01-29 19:34:17 -0500

    MOT17 detector training jobs.

---
+++ jobs/MOT/r18es3e_det.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=14000M
+#SBATCH --gres=gpu:1
+#SBATCH --output="/home/jatin/scratch/slurm-%j.out"
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+
+# module load python/3.9 cuda cudnn scipy-stack opencv
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+# source /home/jatin/ENV/bin/activate
+python /home/jatin/work/tracking_analysis/src/utils/traktor_objdet.py --dataset=/home/jatin/projects/def-cpoullis/jatin/MOT-Challenge/MOT17 --synth_dataset=/home/jatin/projects/def-cpoullis/jatin/MOT-Challenge/Synth --model_prefix=R18eS3e --mode="train-18e" --synthmode="train-3e" --dataset_class=MOT17 --model=/home/jatin/models/R18eS3e_epoch_22.model
+
+exit
