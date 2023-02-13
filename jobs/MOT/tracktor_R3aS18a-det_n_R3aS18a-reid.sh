8c13cd700bf865f0f693ba5cdf8fe7f83f6b9a56
Jatin Katyal <jatinkatyal96@gmail.com>
2023-02-12 19:50:07 -0500

    Mot17 results generation jobs .

---
+++ jobs/MOT/tracktor_R3aS18a-det_n_R3aS18a-reid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=3:59:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=10000M
+#SBATCH --gres=gpu:1
+#SBATCH --output="/home/jatin/scratch/slurm-%j.out"
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+#unzip -q /home/jatin/datasets/VisDrone/VisDrone2019-MOT-test-dev.zip -d $SLURM_TMPDIR/
+
+python /home/jatin/work/tracking_analysis/src/tracktor.py --det_model=/home/jatin/models/R3aS18a_epoch_30.model --reid_model=/home/jatin/models/tracktor_reid_R3aS18a/model/model.pth.tar-10 --config=/home/jatin/work/tracking_analysis/cfgs/tracktor.yaml --dataset=/home/jatin/projects/def-cpoullis/jatin/MOT-Challenge/MOT17 --output=/home/jatin/results/tracktor/MOT/R3aS18a-det_n_R3aS18a-reid  --dataset_type="MOT-train"
+
+exit
---
+++ jobs/MOT/tracktor_R3bS18b-det_n_R3bS18b-reid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --time=3:59:00
+#SBATCH --account=def-cpoullis
+#SBATCH --mem=10000M
+#SBATCH --gres=gpu:1
+#SBATCH --output="/home/jatin/scratch/slurm-%j.out"
+#SBATCH --mail-user=jatinkatyal96@gmail.com
+#SBATCH --mail-type=ALL
+
+module load cuda cudnn gcc/9.3 opencv python/3.9
+
+#unzip -q /home/jatin/datasets/VisDrone/VisDrone2019-MOT-test-dev.zip -d $SLURM_TMPDIR/
+
+python /home/jatin/work/tracking_analysis/src/tracktor.py --det_model=/home/jatin/models/R3bS18b_epoch_30.model --reid_model=/home/jatin/models/tracktor_reid_R3bS18b/model/model.pth.tar-10 --config=/home/jatin/work/tracking_analysis/cfgs/tracktor.yaml --dataset=/home/jatin/projects/def-cpoullis/jatin/MOT-Challenge/MOT17 --output=/home/jatin/results/tracktor/MOT/R3bS18b-det_n_R3bS18b-reid  --dataset_type="MOT-train"
+
+exit
